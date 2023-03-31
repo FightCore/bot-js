1227f502df56dd5b5700693ff4fd749f158335ca
Bart de Bever <bartdebever112@gmail.com>
2023-03-31 09:11:08 +0200

    Await call

--- src/discord-client.ts
+++ src/discord-client.ts
@@ -64,7 +64,7 @@ export class DiscordClient {
       }
 
       if (interaction.isButton() || interaction.isStringSelectMenu()) {
-        componentInteractionHandler.handle(interaction);
+        await componentInteractionHandler.handle(interaction);
       }
     } catch (error) {
       if (interaction.isButton() || interaction.isStringSelectMenu()) {
