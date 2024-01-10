3a9615f346276271a7597ef158d7fedecad7f683
Bart de Bever <bartdebever112@gmail.com>
2024-01-11 00:07:33 +0100

    Local data loading

--- src/data/loader.ts
+++ src/data/loader.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
 import { inject, injectable } from 'inversify';
 import { Logger } from 'winston';
 import { Symbols } from '../config/symbols';
 import { Character } from '../models/character';
+import frameData from '../assets/framedata.json';
 
 @injectable()
 export class Loader {
@@ -17,7 +17,20 @@ export class Loader {
   }
 
   async load(): Promise<void | Character[]> {
-    const result = await axios.get('https://apiv2.fightcore.gg/exports/full');
-    this.characters = result.data as Character[];
+    //const result = await axios.get('https://apiv2.fightcore.gg/exports/full');
+    this.characters = this.removeNulls(frameData) as Character[];
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private removeNulls(obj: any): any {
+    if (obj === null) {
+      return undefined;
+    }
+    if (typeof obj === 'object') {
+      for (const key in obj) {
+        obj[key] = this.removeNulls(obj[key]);
+      }
+    }
+    return obj;
   }
 }
